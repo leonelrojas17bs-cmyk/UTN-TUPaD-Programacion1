4ae9039f610f03934170e8abf9cfbbd725637001
leonelrojas17bs-cmyk <leonelrojas17bs@gmail.com>
2025-08-21 12:57:42 -0300

    Update complementarios.py

--- unidad2 github/complementarios.py
+++ unidad2 github/complementarios.py
@@ -1,28 +1,58 @@
 #actividad de ejercicios complementarios unidad2
-numero1= 5
-numero2= 5.5
+#ejercicio numero 1
+
+numero1 = int(input("Ingresa el primer número: "))
+numero2 = int(input("Ingresa el segundo número: "))
 suma= numero1+numero2
 resta= numero1-numero2
 multiplicacion= numero1*numero2
 division= numero1/numero2
-nombre= "leonel"
-precio= 15.25
+print(f"la suma es de los dos numeros es: {suma}")
+print(f"la resta de los dos numeros es: {resta}")
+print(f"la multiplicacion de los dos numeros es: {multiplicacion}")
+print(f"la division de los dos numeros es: {division}")
+
+#ejercicio descuento y nombre
+
+nombre = input("Ingresa tu nombre: ")
+precio = float(input("Ingresa el precio del producto: "))
 descuento= 0.50
 precio_final= precio*descuento
-cadena= "felicidades, nose por que pero felicidades"
+print(f"hola {nombre}, tiene un descuento de: {precio_final}")
+
+#ejercicio longitud cadena
+
+cadena= input("ingrese una cadena ")
 longitud = len(cadena)
-precio= 20.99
+print(longitud)
+
+#ejercicio convertir a entero
+
+precio= float(input("ingrese un numero con decimales: "))
 precio= int(precio)
-nombre= "leonel"
+print(precio)
+
+#ejercicio nombre + apellido
+
+nombre2= "leonel"
 apellido= "rojas"
-nombre_completo= nombre + " " + apellido
+nombre_completo= nombre2 + " " + apellido
+print(f"{nombre_completo}")
+
+#ejercicio edad y altura modificadas
+
 edad= 18
 edad= edad + 5
 edad= edad - 10
 altura= 1.64
 altura= (altura*4)/3
+print(f"altura: {altura} edad: {edad}")
+
+#nombre a mayusculas y nombre primera letra 
+
 nombremay= "LEONEL ROJAS"
 nombremin= nombremay.lower()
-nombreprimermay= nombremay.capitalize()
-sumar = 1 + 3
+nombreprimermay= nombremin.capitalize()
+print(f"nombre en minusculas: {nombremin} nombre arreglado: {nombreprimermay}")
+
 #fin de la actividad
