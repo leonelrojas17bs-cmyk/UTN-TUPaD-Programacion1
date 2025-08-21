37d21e868976107ce4c2e93eb5133f6761302255
leonelrojas17bs-cmyk <leonelrojas17bs@gmail.com>
2025-08-21 09:01:49 -0300

    subiendo cosas

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # UTN-TUPaD-Programacion1
 repositorio de Leonel Enrique Rojas UTN tecnicatura de programación promoción 4
-este es mi primer cambio
---
+++ unidad2 github/complementarios.py
@@ -0,0 +1,28 @@
+#actividad de ejercicios complementarios unidad2
+numero1= 5
+numero2= 5.5
+suma= numero1+numero2
+resta= numero1-numero2
+multiplicacion= numero1*numero2
+division= numero1/numero2
+nombre= "leonel"
+precio= 15.25
+descuento= 0.50
+precio_final= precio*descuento
+cadena= "felicidades, nose por que pero felicidades"
+longitud = len(cadena)
+precio= 20.99
+precio= int(precio)
+nombre= "leonel"
+apellido= "rojas"
+nombre_completo= nombre + " " + apellido
+edad= 18
+edad= edad + 5
+edad= edad - 10
+altura= 1.64
+altura= (altura*4)/3
+nombremay= "LEONEL ROJAS"
+nombremin= nombremay.lower()
+nombreprimermay= nombremay.capitalize()
+sumar = 1 + 3
+#fin de la actividad
